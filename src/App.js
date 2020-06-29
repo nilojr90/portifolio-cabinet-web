95fc00967f4ed7b73406bfa4e22fdcdff20be522
nilojr90 <nilojr90@gmail.com>
2020-06-29 20:36:18 -0300

    Carregar lista de repositórios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
+import api from "services/api"
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  //trigger once onLoad
+  useEffect(() => {
+    api.get('repositories').then(response =>{
+      setProjects(response.data)
+    });
+  },[]);
+  
+
   async function handleAddRepository() {
     // TODO
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    console.log(id);
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid="repository-list">{
+        projects.map(project => 
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>
+             Remover
+            </button>
+          </li> 
+        )
+      }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
+
+
 
 export default App;
